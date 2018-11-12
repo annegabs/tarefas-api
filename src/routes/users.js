06029035576c10273d7977c1451ef04465e54b61
Anne Gabrielle <annegabs65@gmail.com>
2018-11-12 15:39:25 -0300

    add crud para usuários

--- src/routes/users.js
+++ src/routes/users.js
@@ -21,48 +21,19 @@ const users = [
   },
   {
     method: "GET",
-    path: "/users/",
+    path: "/users",
     handler: (request, reply) => {
       return requestHandler(request, reply);
     }
   },
   {
-    method: "POST",
-    path: "/users",
+    method: "GET",
+    path: "/users/",
     handler: (request, reply) => {
-      try {
-        let { username, email } = JSON.parse(request.payload);
-        if (username === undefined) {
-          username = "";
-          return reply.response({ error: "undefined title" }).code(400);
-        }
-        if (email === undefined) {
-          email = "";
-        }
-        const user = {
-          username: username,
-          email: email,
-          deleted: false,
-          done: false
-        };
-        return knex
-          .into("users")
-          .insert(user)
-          .returning("oid")
-          .then(result => {
-            user.oid = result[0];
-            return reply.response({ status: "inserted", data: user }).code(201);
-          })
-          .catch(err => {
-            return reply.response(err).code(400);
-          });
-      } catch (err) {
-        return reply
-          .response({ error: "undefined user in json object" })
-          .code(400);
-      }
+      return requestHandler(request, reply);
     }
   },
+ 
   {
     method: "PUT",
     path: "/users/{user_id}",
@@ -94,54 +65,7 @@ const users = [
       }
     }
   },
-  {
-    method: "PUT",
-    path: "/users/{user_id}/done",
-    handler: (request, reply) => {
-      try {
-        const id = request.params.user_id;
-        let user = { done: true };
-        return knex("users")
-          .where("oid", id)
-          .update(user)
-          .then(result =>
-            knex("users")
-              .where("oid", id)
-              .select("oid", "username", "email", "deleted", "done")
-              .then(result =>
-                reply.response({ status: "done", data: result[0] }).code(200)
-              )
-          )
-          .catch(err => reply.response(err).code(401));
-      } catch (err) {
-        return reply.response(err).code(401);
-      }
-    }
-  },
-  {
-    method: "PUT",
-    path: "/users/{user_id}/undone",
-    handler: (request, reply) => {
-      try {
-        const id = request.params.user_id;
-        let user = { done: false };
-        return knex("users")
-          .where("oid", id)
-          .update(user)
-          .then(result =>
-            knex("users")
-              .where("oid", id)
-              .select("oid", "username", "email", "deleted", "done")
-              .then(result =>
-                reply.response({ status: "undone", data: result[0] }).code(200)
-              )
-          )
-          .catch(err => reply.response(err).code(401));
-      } catch (err) {
-        return reply.response(err).code(401);
-      }
-    }
-  },
+ 
   {
     method: "DELETE",
     path: "/users/{user_id}",
